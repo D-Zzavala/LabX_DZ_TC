cdafe4464b13dae6e891633ccdf6d78b5cc58ea9
D-Zzavala <65042896+D-Zzavala@users.noreply.github.com>
2020-08-06 19:31:12 -0600

    Push(David Zavala) claseDavid

---
+++ LabX_DZ/src/labx_dz/LabX_DZ.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package labx_dz;
+
+/**
+ *
+ * @author 50431
+ */
+public class LabX_DZ {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
